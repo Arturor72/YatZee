7d391fd4fdc87c379bb3fee96290de54ec462234
arturo <arturor72.jar@gmail.com>
2016-05-12 17:23:41 -0300

    agregando estilos y un dado mas

--- app.rb
+++ app.rb
@@ -3,10 +3,12 @@ require_relative "./lib/Dado.rb"
 get '/' do
 	@@dado=Dado.new
 	@resultado=@@dado.valor
+	@resultado2=@@dado.valor
 	erb :yatzee
 end
 
 post '/lanzarDado' do
 	@resultado=@@dado.lanzar 
+	@resultado2=@@dado.lanzar 
 	erb :yatzee
 end
