9064649720bb474eeecec646aba2e6e5907600d4
lpessagno <leslibethpessagno@gmail.com>
2016-05-12 19:55:37 -0300

    refactoring de clase Yatzee para lanzar 5 o menos dados

--- lib/Yatzee.rb
+++ lib/Yatzee.rb
@@ -19,4 +19,16 @@ class Yatzee
 		@dados[index]
 	end
 
+	def get_dados
+		@dados
+	end
+
+	def lanzar_dados (arreglo_dados=@dados,vals=0)
+		if vals == 0
+			arreglo_dados.each {|dado| dado.lanzar}
+		else
+			arreglo_dados.each_with_index {|dado,index| dado.lanzar(vals[index])}
+		end
+	end
+
 end
--- spec/yatzee_spec.rb
+++ spec/yatzee_spec.rb
@@ -7,15 +7,23 @@ describe "Juego de Yatzee" do
 		juego.total.should == 0
 	end
 
-	it "Dados tienen los valores [1,2,3,4,5], obtener resultado igual a 15 " do
+	it "Todos los Dados tienen valor [1,2,3,4,5], obtener resultado igual a 15 " do
      	juego = Yatzee.new
-		juego.get_dado(0).lanzar 1
-		juego.get_dado(1).lanzar 2
-		juego.get_dado(2).lanzar 3
-		juego.get_dado(3).lanzar 4
-		juego.get_dado(4).lanzar 5
+		juego.lanzar_dados(juego.get_dados,[1,2,3,4,5])
 		juego.sumar_resultados_dados
 		juego.total.should == 15		
+	end
+
+	it "Primer lanzamiento obtuvo [1,2,3,4,5], relanzar los 3 primeros dados y obtener [6,5,4], suma es 24" do
+     	juego = Yatzee.new
+		juego.lanzar_dados(juego.get_dados,[1,2,3,4,5])
+		dado1=juego.get_dado 0
+		dado2=juego.get_dado 1
+		dado3=juego.get_dado 2
+		dados_dummy=[dado1,dado2,dado3]
+		juego.lanzar_dados(dados_dummy,[6,5,4])
+		juego.sumar_resultados_dados
+		juego.total.should == 24		
 	end
 
 
